57442f7692a2c9f805443ddda081d8289130fb58
kirkwood5953 <kylekirkwood5953@gmail.com>
2021-10-11 11:54:42 -0700

    Lab exam stuff

---
+++ Lab2/Lab2/Exercise3/Exercise3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise3
+{
+	public partial class Exercise3 : Form
+	{
+		float randomNum;
+
+		public Exercise3()
+		{
+			InitializeComponent();
+		}
+	}
+}
